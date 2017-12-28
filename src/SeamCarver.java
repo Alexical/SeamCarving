38b260baaf8b0f59fa4c08fc343d4ef3d0c2af83
Alex Lee <phparkle@gmail.com>
2017-12-28 15:23:22 +0800

    Initial commit

---
+++ src/SeamCarver.java
@@ -0,0 +1,47 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+
+    public SeamCarver(Picture picture) {
+        if (picture != null) {
+
+        } else throw new IllegalArgumentException();
+    }
+
+    public Picture picture() {
+        return null;
+    }
+
+    public int width() {
+        return 0;
+    }
+
+    public int height() {
+        return 0;
+    }
+
+    public double energy(int x, int y) {
+        return 0;
+    }
+
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    public void removeHorizontalSeam(int[] seam) {
+        if (seam != null) {
+
+        } else throw new IllegalArgumentException();
+    }
+
+    public void removeVerticalSeam(int[] seam) {
+        if (seam != null) {
+
+        } else throw new IllegalArgumentException();
+    }
+
+}
